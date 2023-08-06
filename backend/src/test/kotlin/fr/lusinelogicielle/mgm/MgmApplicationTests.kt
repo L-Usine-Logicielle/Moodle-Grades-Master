86a849b002a0bc74ac18fdc8ee210496b12e1745
Lucas <lucas@perfeito.fr>
2023-08-06 05:44:37 +0200

    Fixed Junit/SpringBootTest & improved readibility of workflow for deployment

--- backend/src/test/kotlin/fr/lusinelogicielle/mgm/MgmApplicationTests.kt
+++ backend/src/test/kotlin/fr/lusinelogicielle/mgm/MgmApplicationTests.kt
@@ -1,9 +1,7 @@
 package fr.lusinelogicielle.mgm
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class MgmApplicationTests {
 
     @Test
